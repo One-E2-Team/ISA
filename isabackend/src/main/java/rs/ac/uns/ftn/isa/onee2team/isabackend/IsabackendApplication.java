b8112aa2a308f26a6d7e7fce78e38e90e15f9fc4
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2021-01-28 20:42:33 +0100

    spring init, gitignore in top level

---
+++ isabackend/src/main/java/rs/ac/uns/ftn/isa/onee2team/isabackend/IsabackendApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.isa.onee2team.isabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsabackendApplication.class, args);
+	}
+
+}
