db6a13fa2cda3696d353cb01af81c454022d89c6
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2021-02-02 22:53:17 +0100

    working test and app context database conections

--- isabackend/src/test/java/rs/ac/uns/ftn/isa/onee2team/isabackend/IsabackendApplicationTests.java
+++ isabackend/src/test/java/rs/ac/uns/ftn/isa/onee2team/isabackend/IsabackendApplicationTests.java
@@ -2,12 +2,8 @@ package rs.ac.uns.ftn.isa.onee2team.isabackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = {
-  IsabackendApplication.class, 
-  H2JpaConfig.class})
-@ActiveProfiles("test")
+@SpringBootTest
 class IsabackendApplicationTests {
 
 	@Test
