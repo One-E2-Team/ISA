3c7ae412f9de46b708e056e937d9416ad481eab3
BozidarArsic <arsicbozidar98@gmail.com>
2021-02-03 14:08:43 +0100

    Login modal dialog

--- isafrontend/frontend/src/components/Header.vue
+++ isafrontend/frontend/src/components/Header.vue
@@ -1,30 +1,76 @@
 <template>
-    <header class= "header">
-        <div id= "nav">
-            <router-link to="/"> LogIn </router-link>
-            <router-link to="/welcome"> Welcome </router-link>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#/">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#/">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#/welcome">Link</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#/">Pharmacy</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#/">Medicine</a>
+                </li>
+                 <li class="nav-item">
+                <a class="nav-link" href="#/">Working calendar</a>
+                </li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <button v-if='role === "Anon"' type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#loginModal">Sign In</button> 
+                <button v-if='role === "Anon"' class="btn btn-outline-primary ml-3" click="SignUp">Sign up</button>
+                <button v-if='role !== "Anon"' class="btn btn-outline-primary ml-3" click="ShowProfile">Profile</button>
+            </form>
+            </div>
         </div>
-    </header>
+    </nav>
 </template>
 
 <script>
 export default {
     name: "Header",
+    props: ['role'],
 
     data() {
-    return {
-        msg :"Navigation bar" 
+        return {
+            msg :"Navigation bar" 
+        }
+    },
+    methods: {
+        signIn: function(){
+
+        }
     }
 }
 
-}
 
 
 </script>
 
 <style scoped>
     .header {
-        background: rgb(163, 78, 78);
+        background: #333;
         color: #fff;
         text-align: center;
         padding: 10px;
