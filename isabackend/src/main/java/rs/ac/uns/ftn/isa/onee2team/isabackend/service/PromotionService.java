4a7f7048307c3639ce07131ab73849d2f22d2792
Radoš Milićev <40705899+rammba@users.noreply.github.com>
2021-02-03 22:19:50 +0100

    added email notification sender

--- isabackend/src/main/java/rs/ac/uns/ftn/isa/onee2team/isabackend/service/PromotionService.java
+++ isabackend/src/main/java/rs/ac/uns/ftn/isa/onee2team/isabackend/service/PromotionService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import rs.ac.uns.ftn.isa.onee2team.isabackend.model.dtos.NewPromotionDTO;
 import rs.ac.uns.ftn.isa.onee2team.isabackend.model.pharmacy.Pharmacy;
 import rs.ac.uns.ftn.isa.onee2team.isabackend.model.promotions.Promotion;
+import rs.ac.uns.ftn.isa.onee2team.isabackend.model.users.Patient;
 import rs.ac.uns.ftn.isa.onee2team.isabackend.repository.IPharmacyRepository;
 import rs.ac.uns.ftn.isa.onee2team.isabackend.repository.IPromotionRepository;
 
@@ -14,11 +15,13 @@ public class PromotionService implements IPromotionService {
 	
 	private IPromotionRepository promotionRepository;
 	private IPharmacyRepository pharmacyRepository;
+	private EmailNotificationService emailNotificationService;
 	
 	@Autowired
-	public PromotionService(IPromotionRepository promotionRepository, IPharmacyRepository pharmacyRepository) {
+	public PromotionService(IPromotionRepository promotionRepository, IPharmacyRepository pharmacyRepository, EmailNotificationService emailNotificationService) {
 		this.pharmacyRepository = pharmacyRepository;
 		this.promotionRepository = promotionRepository;
+		this.emailNotificationService = emailNotificationService;
 	}
 
 	@Override
@@ -30,6 +33,13 @@ public class PromotionService implements IPromotionService {
 		Pharmacy pharmacy = pharmacyRepository.findById(newPromotion.getPharmacyId()).orElse(null);
 		if(pharmacy != null) {
 			promotion.setPharmacy(pharmacy);
+			String subject = "Pharmacy : " + pharmacy.getId() + " new promotion";
+			for (Patient patient : pharmacy.getSubscribedPatients()) {
+				String mailMessage = "Dear " + patient.getFirstName() + " " + patient.getLastName() + ", pharmacy has added new promotion";
+				try {
+					emailNotificationService.sendNotificaitionAsync("hgstqyssyzfutkvqpk@miucce.com", subject, mailMessage);
+				} catch (Exception e) {}
+			}
 			return promotionRepository.save(promotion);
 		}
 		return null;
