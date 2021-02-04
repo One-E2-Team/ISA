96fc83e447160816b179d09d9a375bbce1e0fc9b
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2021-02-04 22:41:23 +0100

    profile switch on frontend for all roles

--- isafrontend/frontend/src/router.js
+++ isafrontend/frontend/src/router.js
@@ -4,6 +4,12 @@ import VueRouter from 'vue-router'
 
 import Welcome from './components/Welcome'
 import Pharmacy from './components/Pharmacy'
+import Patient from './components/profiles/Patient'
+import Pharmacist from './components/profiles/Pharmacist'
+import Dermatologist from './components/profiles/Dermatologist'
+import PharmacyAdmin from './components/profiles/PharmacyAdmin'
+import SystemAdmin from './components/profiles/SystemAdmin'
+import Dealer from './components/profiles/Dealer'
 
 Vue.use(VueRouter)
 
@@ -19,6 +25,36 @@ const router = new VueRouter({
       path: '/pharmacy',
       name: 'pharmacy',
       component: Pharmacy
+    },
+    {
+      path: '/patient',
+      name: 'patient',
+      component: Patient
+    },
+    {
+      path: '/pharmacist',
+      name: 'pharmacist',
+      component: Pharmacist
+    },
+    {
+      path: '/dermatologist',
+      name: 'dermatologist',
+      component: Dermatologist
+    },
+    {
+      path: '/pharmacy-admin',
+      name: 'pharmacy-admin',
+      component: PharmacyAdmin
+    },
+    {
+      path: '/system-admin',
+      name: 'system-admin',
+      component: SystemAdmin
+    },
+    {
+      path: '/dealer',
+      name: 'dealer',
+      component: Dealer
     }
   ]
 })
