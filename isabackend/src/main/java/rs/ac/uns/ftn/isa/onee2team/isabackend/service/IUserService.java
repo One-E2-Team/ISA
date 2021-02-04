3dc8bc40c23197b51fbdfc034aa3319199aac5ab
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2021-02-04 21:02:26 +0100

    merge conflict resolve on user service

--- isabackend/src/main/java/rs/ac/uns/ftn/isa/onee2team/isabackend/service/IUserService.java
+++ isabackend/src/main/java/rs/ac/uns/ftn/isa/onee2team/isabackend/service/IUserService.java
@@ -2,9 +2,9 @@ package rs.ac.uns.ftn.isa.onee2team.isabackend.service;
 
 import java.util.List;
 
-import rs.ac.uns.ftn.isa.onee2team.isabackend.model.dtos.UserRequestDTO
 import rs.ac.uns.ftn.isa.onee2team.isabackend.model.dtos.FirstLastNameDTO;
 import rs.ac.uns.ftn.isa.onee2team.isabackend.model.dtos.HealthWorkerDTO;
+import rs.ac.uns.ftn.isa.onee2team.isabackend.model.dtos.UserRequestDTO;
 import rs.ac.uns.ftn.isa.onee2team.isabackend.model.users.Patient;
 import rs.ac.uns.ftn.isa.onee2team.isabackend.model.users.User;
 
@@ -14,7 +14,6 @@ public interface IUserService {
 	User createPatient(UserRequestDTO userRequest);
 	List<HealthWorkerDTO> getAllPharmacistsByFirstAndLastName(FirstLastNameDTO firstAndLastName);
 	List<HealthWorkerDTO> getAllDermatologistsByFirstAndLastName(FirstLastNameDTO firstAndLastName);
-	User save(UserRequest userRequest);
 	User findByEmail(String email);
 	User saveUser(User user);
 	User findById(Long id);
