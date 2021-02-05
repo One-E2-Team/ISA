2fbe0e3e27a5df829135c442fc1d4e00e92222ae
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2021-02-05 21:10:56 +0100

    backend loyalty for systemadmin

--- isabackend/src/main/java/rs/ac/uns/ftn/isa/onee2team/isabackend/service/PromotionService.java
+++ isabackend/src/main/java/rs/ac/uns/ftn/isa/onee2team/isabackend/service/PromotionService.java
@@ -1,12 +1,17 @@
 package rs.ac.uns.ftn.isa.onee2team.isabackend.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import rs.ac.uns.ftn.isa.onee2team.isabackend.model.dtos.LoyaltyDTO;
 import rs.ac.uns.ftn.isa.onee2team.isabackend.model.dtos.NewPromotionDTO;
 import rs.ac.uns.ftn.isa.onee2team.isabackend.model.pharmacy.Pharmacy;
+import rs.ac.uns.ftn.isa.onee2team.isabackend.model.promotions.Loyalty;
 import rs.ac.uns.ftn.isa.onee2team.isabackend.model.promotions.Promotion;
 import rs.ac.uns.ftn.isa.onee2team.isabackend.model.users.Patient;
+import rs.ac.uns.ftn.isa.onee2team.isabackend.repository.ILoyaltyRepository;
 import rs.ac.uns.ftn.isa.onee2team.isabackend.repository.IPharmacyRepository;
 import rs.ac.uns.ftn.isa.onee2team.isabackend.repository.IPromotionRepository;
 
@@ -16,12 +21,14 @@ public class PromotionService implements IPromotionService {
 	private IPromotionRepository promotionRepository;
 	private IPharmacyRepository pharmacyRepository;
 	private IEmailNotificationService emailNotificationService;
+	private ILoyaltyRepository loyaltyRepository;
 	
 	@Autowired
-	public PromotionService(IPromotionRepository promotionRepository, IPharmacyRepository pharmacyRepository, IEmailNotificationService emailNotificationService) {
+	public PromotionService(IPromotionRepository promotionRepository, IPharmacyRepository pharmacyRepository, IEmailNotificationService emailNotificationService, ILoyaltyRepository loyaltyRepository) {
 		this.pharmacyRepository = pharmacyRepository;
 		this.promotionRepository = promotionRepository;
 		this.emailNotificationService = emailNotificationService;
+		this.loyaltyRepository = loyaltyRepository;
 	}
 
 	@Override
@@ -42,4 +49,20 @@ public class PromotionService implements IPromotionService {
 		}
 		return null;
 	}
+
+	@Override
+	public Loyalty save(LoyaltyDTO ldto) {
+		Loyalty l = loyaltyRepository.findAllByType(ldto.getType()).get(0);
+		l.setDiscount(ldto.getDiscount());
+		l.setExaminationPoints(ldto.getExaminationPoints());
+		l.setMinPoints(ldto.getMinPoints());
+		return this.loyaltyRepository.save(l);
+	}
+
+	@Override
+	public List<Loyalty> getAllLoyalties() {
+		return this.loyaltyRepository.findAll();
+	}
+	
+	
 }
