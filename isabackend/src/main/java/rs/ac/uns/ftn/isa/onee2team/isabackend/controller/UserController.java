2e9d2b85a5823f263aaa3510cf47c8792516faa0
BozidarArsic <arsicbozidar98@gmail.com>
2021-02-09 23:38:06 +0100

    price for medicine

--- isabackend/src/main/java/rs/ac/uns/ftn/isa/onee2team/isabackend/controller/UserController.java
+++ isabackend/src/main/java/rs/ac/uns/ftn/isa/onee2team/isabackend/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -173,8 +174,8 @@ public class UserController {
 		return userService.getDermatologistsWhoAreNotInPharmacy(user.getId());
 	}
 	
-	@GetMapping(value ="/patient-allergies-ids/")
-	public List<Long> getPatientAllergiesIds(@PathParam("id") Long patientId){
+	@GetMapping(value ="/patient-allergies-ids/{id}")
+	public List<Long> getPatientAllergiesIds(@PathVariable("id") Long patientId){
 		return userService.getPatientAllergiesIds(patientId);
 	}
 }
