576252ded463419f299a8442a7c4a3046ae7861e
BozidarArsic <arsicbozidar98@gmail.com>
2021-02-12 22:14:31 +0100

    show reservation data in dialog

--- isafrontend/frontend/src/components/modals/TakeReservedMedicineModal.vue
+++ isafrontend/frontend/src/components/modals/TakeReservedMedicineModal.vue
@@ -3,16 +3,22 @@
         <div class="modal-dialog">
             <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" >Start therapy</h5>
+                <h5 class="modal-title" >Reservation</h5>
                 <button ref="closeBtn" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <label for="reservationInput" class="form-label">Reservation id:</label>
-                <input type="text" v-model="reservationId" class="form-control" id="reservationInput">
+                <input type="text" v-model="reservationId" class="form-control">
+                <button class="btn btn-primary" @click="search()">Search</button>
+                <div ref="medInfo" class="mb-3 d-none container"><br/>
+                    <label class="form-label">Medicine: {{medicine.name}}</label><br/>
+                    <label class="form-label">Manufacturer: {{medicine.manufacturer}}</label><br />
+                    <label class="form-label">Code: {{medicine.code}}</label>
+                </div> 
             </div>
             <div class="modal-footer">
                 <button type="button" data-bs-dismiss="modal" class="btn btn-danger" >Cancel</button>
-                <button type="button" @click="takeReservation()" class="btn btn-primary">Take</button>
+                <button type="button" @click="takeReservation()" ref="take" class="btn btn-primary" disabled>Take</button>
             </div>
             </div>
         </div>
@@ -28,9 +34,26 @@ export default {
     data(){
         return{
             reservationId: "",
+            medicine: {}
         }
     },
     methods: {
+        search: function(){
+            axios.get('http://' + comm.server + '/api/reservations/search/medicine?reservationId='+this.reservationId)
+                .then(response=>{
+                    if(response.data==null || response.data==""){
+                        this.medicine={};
+                        this.$vToastify.error("Reservation "+this.reservationId+" doesn't exist");
+                        this.$refs.medInfo.classList.add("d-none");
+                        this.$refs.take.disabled = true;
+                    }
+                    else{
+                        this.medicine=response.data;
+                        this.$refs.medInfo.classList.remove("d-none");
+                        this.$refs.take.disabled = false;
+                    }
+                })
+        },
         takeReservation: function(){
             axios.post('http://' + comm.server + '/api/reservations/takeMedicine?reservationId='+this.reservationId)
                 .then(response=>{
