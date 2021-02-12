f12d6a4a5274029b694f7e8933abeac1bf6e1753
BozidarArsic <arsicbozidar98@gmail.com>
2021-02-12 22:34:13 +0100

    now vacation request is fabulous

--- isafrontend/frontend/src/components/pages/VacationRequestPage.vue
+++ isafrontend/frontend/src/components/pages/VacationRequestPage.vue
@@ -1,17 +1,35 @@
 <template>
     <div>
-        <input type="date" v-model="startDate" />
-        <input type="date" v-model="endDate" />
-        <label for="reason">Reason:</label>
+        <br/>
+        <div>
+        <date-picker v-model="range" is-range :min-date="new Date()">
+                <template v-slot="{ inputValue, inputEvents }">
+                    <div class="flex justify-center items-center">
+                    <input
+                        :value="inputValue.start"
+                        v-on="inputEvents.start"
+                        class="border px-2 py-1 w-32 rounded focus:outline-none focus:border-indigo-300"/>
+                    ->
+                    <input
+                        :value="inputValue.end"
+                        v-on="inputEvents.end"
+                        class="border px-2 py-1 w-32 rounded focus:outline-none focus:border-indigo-300"
+                    />
+                    <label for="reason" class="mx-3"> Reason: </label>
 
-        <select id="reason" v-model="reason">
-            <option value="VACATION" selected >Vacation</option>
-            <option value="SABBATICAL">sabbatical</option>
-            <option value="MATERNITY">maternity</option>
-            <option value="SICK">sick</option>
-        </select>
+                    <select id="reason" v-model="reason">
+                        <option value="VACATION" selected >Vacation</option>
+                        <option value="SABBATICAL">Sabbatical</option>
+                        <option value="MATERNITY">Maternity</option>
+                        <option value="SICK">Sick</option>
+                    </select>
 
-        <button @click="requestVacation">Send request</button>
+                    <button class="btn btn-primary mx-3" type="button" @click="requestVacation">Send request</button>
+                    </div>
+                </template>
+        </date-picker>
+        
+        </div>
     </div>
 </template>
 
@@ -25,22 +43,25 @@ export default {
     name : 'VacationRequestPage',
     data(){
         return {
-            startDate: "",
-            endDate : "",
-            reason : ""
+            range: {
+                start: new Date(),
+                end: new Date(),
+            },
+            reason : "VACATION"
         }
     },
     methods: {
         requestVacation: function(){
-            var start = new Date(this.startDate);
-            var end = new Date(this.endDate);
             var request = {
-                "start" : start,
-                "end" : end,
+                "start" : this.range.start,
+                "end" : this.range.end,
                 "type" : this.reason
             }
             axios.post('http://' + comm.server + '/api/calendar/vacation',request).then(response=>{
-                console.log(response);
+                if(response.status == 200)
+                    this.$vToastify.success("Vacation request successfully sent ");
+                else
+                    this.$vToastify.error("vacation request not sent successfully ");
             }
             )
         }
