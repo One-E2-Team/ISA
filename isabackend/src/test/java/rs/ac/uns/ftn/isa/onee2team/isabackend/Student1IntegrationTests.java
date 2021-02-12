0df386e4df2673f49e3ede5af2f4dfc69985a0fd
andrej.1998 <andrej.1998@uns.ac.rs>
2021-02-12 21:48:50 +0100

    added integration tests

--- isabackend/src/test/java/rs/ac/uns/ftn/isa/onee2team/isabackend/Student1IntegrationTests.java
+++ isabackend/src/test/java/rs/ac/uns/ftn/isa/onee2team/isabackend/Student1IntegrationTests.java
@@ -1,17 +1,28 @@
 package rs.ac.uns.ftn.isa.onee2team.isabackend;
 
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 
-
-import rs.ac.uns.ftn.isa.onee2team.isabackend.model.dtos.LoyaltyDTO;
-import rs.ac.uns.ftn.isa.onee2team.isabackend.model.promotions.CategoryType;
-import rs.ac.uns.ftn.isa.onee2team.isabackend.service.IPromotionService;
+import rs.ac.uns.ftn.isa.onee2team.isabackend.model.dtos.NewRateDTO;
+import rs.ac.uns.ftn.isa.onee2team.isabackend.model.dtos.RequestReservationDTO;
+import rs.ac.uns.ftn.isa.onee2team.isabackend.model.dtos.ReservedMedicineDTO;
+import rs.ac.uns.ftn.isa.onee2team.isabackend.model.dtos.ScheduledExaminationDTO;
+import rs.ac.uns.ftn.isa.onee2team.isabackend.model.users.Patient;
+import rs.ac.uns.ftn.isa.onee2team.isabackend.service.IExaminationService;
+import rs.ac.uns.ftn.isa.onee2team.isabackend.service.IFeedbackService;
+import rs.ac.uns.ftn.isa.onee2team.isabackend.service.IMedicineReservationService;
+import rs.ac.uns.ftn.isa.onee2team.isabackend.service.IUserService;
 
 @ContextConfiguration(classes = IsabackendApplication.class)
 @SpringBootTest
@@ -19,36 +30,68 @@ import rs.ac.uns.ftn.isa.onee2team.isabackend.service.IPromotionService;
 public class Student1IntegrationTests {
 
 	@Autowired
-	IPromotionService promotionService;
+	IMedicineReservationService medicineReservationService;
+	
+	@Autowired
+	IUserService userService;
+	
+	@Autowired
+	IFeedbackService feedbackService;
+	
+	@Autowired
+	IExaminationService examinationService;
 	
 	@Test()
-	void allPatientsTest() {
+	@Sql(scripts = {"/cancelReservationdata.sql"})
+	void cancelReservationTest() {
 		
-		/*
-		System.out.println("111");
+		medicineReservationService.cancelReservation(1L, 1L);
+			
+		List<ReservedMedicineDTO> lista = medicineReservationService.getPatientsReservations(1L);
 		
-		LoyaltyDTO dto = new LoyaltyDTO();
-		dto.setType(CategoryType.SILVER);
-		dto.setDiscount(0.2);
-		dto.setExaminationPoints(3);
-		dto.setMinPoints(20);
-		
-		promotionService.save(dto);*/
+		assertEquals(0, lista.size());
 	}
 	
+	/*
+	@SuppressWarnings("deprecation")
+	@Test()
+	void idk() {
+		List<ScheduledExaminationDTO> lista = examinationService.getPatientsExaminations(1L);
+		assertEquals(5.0, lista.get(0).getDoctorRate());
+	}*/
+	
+	
+	/*
 	@Test
-	void allPatientsTest2() {
+	@Sql(scripts = {"/reserveMedicinedata.sql"})
+	void reserveMedicineTest() {
+		RequestReservationDTO dto = new RequestReservationDTO();
+		dto.setExpireDate(new Date(System.currentTimeMillis()));
+		dto.setMedicine_id(1L);
+		dto.setPharmacy_id(1L);
 		
-		/*
-		System.out.println("222");
+		medicineReservationService.reserve(dto, 1L);
 		
-		LoyaltyDTO dto = new LoyaltyDTO();
-		dto.setType(CategoryType.SILVER);
-		dto.setDiscount(0.2);
-		dto.setExaminationPoints(3);
-		dto.setMinPoints(20);
+		assertEquals(1, medicineReservationService.getPatientsReservations(1L).size());
+	}*/
+	
+	/*
+	@Test
+	@Sql(scripts = {"/ratedata.sql"})
+	void rateMedicineTest() {
+		NewRateDTO dto = new NewRateDTO();
+		dto.setRate(3);
+		dto.setRateEntityId(1L);
+		dto.setRateEntityName("lek 1");
+		dto.setRateEntityType("MEDICINE");
 		
-		promotionService.save(dto);*/
-	}
+		Patient p = (Patient) userService.findById(1L);
+		
+		feedbackService.rate(dto, p);
+		
+		List<NewRateDTO> list = medicineReservationService.getMedicinesForRate(1L);
+		
+		assertEquals(3, (int)list.get(0).getRate());
+	}*/
 }
 
